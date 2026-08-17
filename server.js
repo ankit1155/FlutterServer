41ced6ebda99bbaa35c4a96c94ba7fb82bb1328e
ankit1155 <ankitsaini0055@gmail.com>
2026-08-17 15:21:33 +0530

    Implement WebSocket server on port 8080

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 
+const WebSocket = require("ws");
+
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("message", (message) => {
+    console.log("Received:", message.toString());
+    socket.send(message.toString());
+  });
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("WebSocket server running on port 8080");
