4aabbb91fc77a7bc515ecb5d19c4c9687a1dfc3a
ankit1155 <ankitsaini0055@gmail.com>
2026-08-18 11:44:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const WebSocket = require("ws");
 
 const server = new WebSocket.Server({ port: 8080 });
@@ -7,8 +6,16 @@ server.on("connection", (socket) => {
   console.log("Client connected");
 
   socket.on("message", (message) => {
-    console.log("Received:", message.toString());
-    socket.send(message.toString());
+    const data = message.toString();
+
+    console.log("Received:", data);
+
+    // Send message to all connected clients
+    server.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
   });
 
   socket.on("close", () => {
